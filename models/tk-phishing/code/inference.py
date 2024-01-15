2250dedec5b45774c5693ef85be0a93e56b04b14
Eslam Odeh <eslamodeh@outlook.com>
2024-01-15 17:08:54 +0000

    Update inference and deploy note

--- models/tk-phishing/code/inference.py
+++ models/tk-phishing/code/inference.py
@@ -1,7 +1,15 @@
 
 # https://github.com/mobassir94/Deploy-trained-TensorFlow-2.x-models-using-Amazon-SageMaker/blob/main/inference.py
+# https://docs.aws.amazon.com/sagemaker/latest/dg/neo-deployment-hosting-services-prerequisites.html
 
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+# rest of the inference script from here
 
 def input_handler(data, context):
     """ Pre-process request input before it is sent to TensorFlow Serving REST API
@@ -12,21 +20,20 @@ def input_handler(data, context):
         (dict): a JSON-serializable dict that contains request body and headers
     """
     if context.request_content_type == 'application/json':
+        payload = data.read().decode('utf-8')
+        logger.info("From input handler {}".format(json.dumps(payload)))
+
         # pass through json (assumes it's correctly formed)
         d = data.read().decode('utf-8')
         return d if len(d) else ''
 
-    if context.request_content_type == 'text/csv':
-        # very simple csv handler
-        return json.dumps({
-            'instances': [float(x) for x in data.read().decode('utf-8').split(',')]
-        })
-
-    raise ValueError('{{"error": "unsupported content type {}"}}'.format(
-        context.request_content_type or "unknown"))
+    raise ValueError("Wrong input format given {}. Allowed format application/json").format(
+        context.request_content_type or "unknown")
 
 
 def output_handler(data, context):
+    logger.info("From output handler: {}".format(json.dumps(data)))
+
     """Post-process TensorFlow Serving output before it is returned to the client.
     Args:
         data (obj): the TensorFlow serving response
